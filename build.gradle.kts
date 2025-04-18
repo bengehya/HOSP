5918e2f8daa0d899785dcf811aff899ea7774fc6
bengehya <princebengehya@gmail.com>
2025-04-18 23:36:58 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-
 plugins {
     alias(libs.plugins.android.application) apply false
-  id("org.sonarqube") version "6.0.1.5171"
+    id("org.sonarqube") version "6.0.1.5171"
 }
-    
+
 sonar {
-  properties {
-    property("sonar.projectKey", "Host")
-    property("sonar.projectName", "Host")
-  }
+    properties {
+        property("sonar.projectKey", "Host") // remplace "Host" si besoin
+        property("sonar.projectName", "Host")
+        property("sonar.host.url", "http://localhost:9000")
+        property("sonar.login", "TON-SONAR-TOKEN-ICI") // ton vrai token
+        property("sonar.sources", "app/src/main/java") // tes sources ici
+        property("sonar.tests", "app/src/test/java") // tes tests si tu en as
+    }
 }
