cfa9938baab4e290fedaeb75ee806455121be70c
bengehya <princebengehya@gmail.com>
2025-04-18 23:43:29 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ sonar {
         property("sonar.projectKey", "Host") // remplace "Host" si besoin
         property("sonar.projectName", "Host")
         property("sonar.host.url", "http://localhost:9000")
-        property("sonar.login", "TON-SONAR-TOKEN-ICI") // ton vrai token
+        property("sonar.login", "SONAR-TOKEN") // ton vrai token
         property("sonar.sources", "app/src/main/java") // tes sources ici
         property("sonar.tests", "app/src/test/java") // tes tests si tu en as
     }
