238680c2382d3c4bcbd7a9656be6b14f9e28ba4e
bengehya <princebengehya@gmail.com>
2025-04-19 22:21:55 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,33 @@
+// build.gradle.kts (à la racine)
+
 plugins {
+    id("com.android.application") version "8.2.2" apply false
     id("org.sonarqube") version "6.0.1.5171"
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.application")) {
+            extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(34)
+
+                defaultConfig {
+                    applicationId = "com.example.hospital"
+                    minSdk = 24
+                    targetSdk = 34
+                    versionCode = 1
+                    versionName = "1.0"
+                }
+            }
+        }
+    }
 }
 
 sonar {
