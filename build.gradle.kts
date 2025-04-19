70fb77bfae5366f6b4b4aab31a65e7e597bee58a
bengehya <princebengehya@gmail.com>
2025-04-19 21:42:10 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,13 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
     id("org.sonarqube") version "6.0.1.5171"
 }
 
 sonar {
     properties {
-        property("sonar.projectKey", "Host") // remplace "Host" si besoin
+        property("sonar.projectKey", "Host")
         property("sonar.projectName", "Host")
         property("sonar.host.url", "http://localhost:9000")
-        property("sonar.login", "SONAR-TOKEN") // ton vrai token
-        property("sonar.sources", "app/src/main/java") // tes sources ici
-        property("sonar.tests", "app/src/test/java") // tes tests si tu en as
+        property("sonar.login", System.getenv("SONAR_TOKEN")) // 💥 Utilise l'env var SONAR_TOKEN
     }
 }
+
