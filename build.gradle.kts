3568f4d22959e111f878c6815956ee3910ba6e8c
bengehya <princebengehya@gmail.com>
2025-04-19 21:50:19 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,13 @@
 plugins {
     id("org.sonarqube") version "6.0.1.5171"
+    // Tes autres plugins android ou kotlin peuvent rester
 }
 
-sonar {
+sonarqube {
     properties {
         property("sonar.projectKey", "Host")
         property("sonar.projectName", "Host")
         property("sonar.host.url", "http://localhost:9000")
         property("sonar.login", System.getenv("SONAR_TOKEN"))
-    }
-}
-
-// Si tu veux éviter que le plugin Android bloque :
-tasks.register("sonar") {
-    doLast {
-        println("Running Sonar Analysis (without Android build)")
     }
 }
