67d112ce76a57dff812ac491ca7e295a87798b70
bengehya <princebengehya@gmail.com>
2025-04-19 22:45:01 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,22 @@
 plugins {
-    id("com.android.application") version "8.8.0"
     id("org.sonarqube") version "4.4.1.3373"
-    kotlin("android") version "1.9.20"
+    kotlin("jvm") version "1.9.22"
 }
 
-
-android {
-    namespace = "com.example.hospital"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.hospital"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
+repositories {
+    mavenCentral()
 }
 
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+// Configuration SonarQube
 sonarqube {
     properties {
-        property("sonar.projectKey", "hospital")
+        property("sonar.projectKey", "HOSP")
+        property("sonar.organization", "your-organization-key")
         property("sonar.host.url", "http://localhost:9000")
-        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.login", System.getenv("SONAR_TOKEN"))
     }
 }
